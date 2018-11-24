7c570678fa01a3063c15ac73a8e8a5b6a2263f16
mikezks <michael.zikes@intauria.com>
2018-11-24 14:25:18 +0100

    Services

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Flight } from '../entities/flight';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { FlightService } from './flight.service';
 
 @Component({
   selector: 'app-flight-search',
@@ -13,27 +13,17 @@ export class FlightSearchComponent implements OnInit {
   flights: Flight[] = [];
   selectedFlight: Flight;
 
-  constructor(private http: HttpClient) { }
+  constructor(private flightService: FlightService) { }
 
   ngOnInit(): void {
   }
 
   search(): void {
-    const url = 'http://www.angular.at/api/flight';
-
-    const headers = new HttpHeaders()
-      .set('Accept', 'application/json');
-
-    const params = new HttpParams()
-      .set('from', this.from)
-      .set('to', this.to);
-
-    this.http
-      .get<Flight[]>(url, { params, headers })
+    this.flightService
+      .find(this.from, this.to)
       .subscribe(
         flights => {
           this.flights = flights;
-          console.log(this.flights);
         },
         err => {
           console.error('Error loading flights', err);
